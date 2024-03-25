c4d1da134ed58c3d0dd8aa448392f901290a7fb9
adrianelca16 <adrian.elca15@gmail.com>
2024-03-25 17:00:16 -0400

    le falata los parametros

--- ReporteBac.py
+++ ReporteBac.py
@@ -1,6 +1,7 @@
 from Reporte import *
 from PyQt5 import QtWidgets
 import sys
+import subprocess
 
 class MiApp(QtWidgets.QMainWindow,):
     def __init__(self):
@@ -28,9 +29,17 @@ class MiApp(QtWidgets.QMainWindow,):
 
 
     def irImprimir(self):
+        comandoLista = "scevtls -d postgresql:// --begin 01-01-2024 00:00:00 > lista_de_eventos.txt"
+        subprocess.call(comandoLista, shell=True)
+        with open("lista_de_eventos.txt","r") as archivo:
+            ultimo_sismo = archivo.readlines()[-1]
+        comandoXml = "scxmldump -d postgresql:// -E " + ultimo_sismo + " -PAMF -o ultimo_evento.xml"
+        subprocess.call(comandoXml,shell=True)
+        comandoBoletin = "scbulletin -i /home/ndcuser/reporteautomatico/ultimo_evento.xml > /home/ndcuser/reporteautomatico/bulletin_ultimo_sismo.txt"
+        subprocess.call(comandoBoletin,shell=True)
         self.ui.stackedWidget_izq.setCurrentIndex(1)
         self.ui.stackedWidget_der.setCurrentIndex(1)
-
+ 
         
     def volver(self):
         self.ui.stackedWidget_izq.setCurrentIndex(0)
--- leer.py
+++ leer.py
@@ -1,7 +1,10 @@
+import subprocess
+
+
 with open ('bulletin_ultimo_evento.txt', 'r') as archivo:
     lineas = archivo.readlines()
     linea = lineas[5].strip().split()
-    magnitud = linea[4]
+    magnitud = linea[4].split("=")[1]
     latitud = linea[7]
     longitud = linea[9]
     profundidad = linea[11]
@@ -9,3 +12,5 @@ print ('magnitud: ', magnitud)
 print ('latitud: ', latitud)
 print ('longitud: ', longitud)
 print ('profundidad: ', profundidad)
+
+subprocess.run(['python', 'Azimut.py', '--mag', str(magnitud), '--lat', str(latitud), '--lon',str(longitud), '--profundidad',str(profundidad)])  # Ejecuta el
