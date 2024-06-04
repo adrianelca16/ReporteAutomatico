4bb5e260103126eeccf6d1d3b0b026adc74f7ee3
adrianelca16 <adrian.elca15@gmail.com>
2024-06-04 11:37:09 -0400

    modificando Leer.PY

--- leer.py
+++ leer.py
@@ -5,32 +5,28 @@ import sys
 python_path = sys.executable
 
 with open('bulletin_ultimo_evento.txt', 'r') as archivo:
-    texto = archivo.read()
+    texto = archivo.readlines()[:7]
+
+    texto1=texto[5].strip()
+
+    lista = texto1.split()
+
+    cantidad = len(lista)
+
+    profundidad = lista[cantidad - 2]
+    longitud = lista[cantidad - 4]
+    latitud = lista[cantidad - 6]
+    hora = lista[cantidad - 7]
+    fecha = lista[cantidad - 8]
+    magnitudTexto = lista[cantidad - 9]
+    magnitud = magnitudTexto.split("=")[1]
+
 
 # Usamos expresiones regulares para buscar los valores que necesitamos
-patron = r"MLv=(\d+\.\d+).*?(\d+\.\d+)\s([NS])\s+(\d+\.\d+)\s([WE]).*?(\d+\s+km).*?(\d+/\d+/\d+\s+\d+:\d+:\d+\.\d+)"
 
-resultado = re.search(patron, texto, re.DOTALL)
 
-if resultado:
-    magnitud = resultado.group(1)
-    latitud = resultado.group(2)
-    longitud = resultado.group(4)
-    profundidad_texto = resultado.group(6)
-    fecha_hora = resultado.group(7)
-
-    # Dividir la cadena de fecha y hora
-    fecha, hora = fecha_hora.split()
-
-    # Extraer solo el valor numérico de la profundidad
-    profundidad_numerica = re.search(r"(\d+)\s+km", profundidad_texto)
-    if profundidad_numerica:
-        profundidad = float(profundidad_numerica.group(1))
-    else:
-        print("No se pudo extraer la profundidad del formato esperado.")
-        profundidad = 0.0
+print(profundidad, latitud, longitud, hora,fecha, magnitud)
 
     # Llamar a otro script con los valores extraídos
-    subprocess.call([python_path, 'Azimut.py', '--mag', magnitud, '--lat', f"{latitud}", '--lon', f"{longitud}", '--pro', str(profundidad), '--fec', fecha, '--hor', hora])
-else:
-    print("No se encontraron los valores necesarios en el archivo.")
+subprocess.call([python_path, 'Azimut.py', '--mag', magnitud, '--lat', f"{latitud}", '--lon', f"{longitud}", '--pro', str(profundidad), '--fec', fecha, '--hor', hora])
+
